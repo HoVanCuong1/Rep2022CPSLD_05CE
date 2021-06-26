922e0bd0bce66c08a592ca948f09b8dfe5667fdd
Trieu Than <trieuthan92pro@gmail.com>
2021-06-26 19:06:56 +0700

    Add ex l83

---
+++ Lesson 83/Ex8/Subject.hpp
@@ -0,0 +1,37 @@
+/**
+ * @author Branium Academy
+ * @website braniumacademy.net
+ * @version 2021.06
+ *
+ */
+
+#ifndef Subject_hpp
+#define Subject_hpp
+#include <iostream>
+namespace Exercises8 {
+    class Subject {
+        friend std::ostream& operator << (std::ostream&, const Subject&);
+        friend std::istream& operator >> (std::istream&, Subject&);
+        char id[20]; // mã môn
+        char name[40]; // tên môn học
+        int lesson; // số tiết học
+        int credit; // số tín chỉ
+    public:
+        Subject() {}
+        Subject(std::string id) { setId(id); }
+        Subject(std::string id, std::string name, int lesson, int credit);
+        void setId(std::string id);
+        void setName(std::string name);
+        void setLesson(int lesson) { this->lesson = lesson; }
+        void setCredit(int credit) { this->credit = credit; }
+        std::string getId() const { return id; }
+        std::string getName() const { return name; }
+        int getCredit() const { return credit; }
+        int getLesson() const { return lesson; }
+        const bool operator == (const Subject&) const;
+        const bool operator < (const Subject&) const;
+        const bool operator <= (const Subject&) const;
+    };
+
+}
+#endif /* Subject_hpp */
