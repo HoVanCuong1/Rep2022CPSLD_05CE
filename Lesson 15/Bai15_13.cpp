cf1d1911b4d4ba8e8b7a1573092ccfba262f4baf
Trieu Than <trieuthan92pro@gmail.com>
2021-07-17 17:40:18 +0700

    Add ex15 l15

---
+++ Lesson 15/Bai15_13.cpp
@@ -0,0 +1,18 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if (n <= 0) {
+		cout << "INVALID" << endl;
+	}
+	else {
+		int i = n;
+		for (; i > 9; i /= 10)
+		{
+		} // sau khi vòng lặp kết thúc, i sẽ là giá trị cần tìm
+		cout << i << endl;
+	}
+	return 0;
+}
