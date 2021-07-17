70b95673d50fdeb89c2b71186e6ea1f04045807a
Trieu Than <trieuthan92pro@gmail.com>
2021-07-17 17:16:34 +0700

    Add ex14 l15

---
+++ Lesson 15/Bai15_14.cpp
@@ -0,0 +1,19 @@
+﻿#include<iostream>
+using namespace std;
+
+int main() {
+	int n;
+	cin >> n;
+	if (n <= 0) {
+		cout << "INVALID" << endl;
+	}
+	else {
+		int product = 1; // giả định ban đầu tích = 1
+		for (int i = n; i > 0; i /= 10)
+		{
+			product *= i % 10; // i%10 sẽ lấy được phần đơn vị của i
+		}
+		cout << product << endl;
+	}
+	return 0;
+}
